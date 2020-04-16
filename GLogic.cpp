33edda1eb23dbb88196d0f00c282a292ac8d903f
SamMcCullough1213 <61021204+SamMcCullough1213@users.noreply.github.com>
2020-04-16 14:18:00 +0100

    Add files via upload

---
+++ GLogic.cpp
@@ -0,0 +1,34 @@
+#include "GLogic.h"
+#include "Pieces.h"
+#include <cstdlib>
+
+GLogic::GLogic()
+{
+
+
+
+}
+
+void GLogic::Validmove(int X, int Y)
+{
+	pieces pieces;
+
+	if (boardArray[X][Y] = NULL || pieces.bR || pieces.bN || pieces.bB || pieces.bQ || pieces.bB2 || pieces.bN2 || pieces.bR2 || pieces.bP1 || pieces.bP2 || pieces.bP3 || pieces.bP4 || pieces.bP5 || pieces.bP6 || pieces.bP7 || pieces.bP8);
+		validMove = true;
+	if (boardArray[X][Y] = pieces.bK)
+		done = true;
+
+}
+
+void GLogic::ValidmovePawn(int X, int Y)
+{
+	pieces pieces;
+
+	if (boardArray[X][Y] = pieces.bR || pieces.bN || pieces.bB || pieces.bQ || pieces.bB2 || pieces.bN2 || pieces.bR2 || pieces.bP1 || pieces.bP2 || pieces.bP3 || pieces.bP4 || pieces.bP5 || pieces.bP6 || pieces.bP7 || pieces.bP8);
+	validMove = true;
+	if (boardArray[X][Y] = pieces.bK)
+		done = true;
+
+}
+
+
