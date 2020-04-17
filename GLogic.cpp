82789a6d935564dbaed4fd048a9032d7ba6039a5
SamMcCullough1213 <61021204+SamMcCullough1213@users.noreply.github.com>
2020-04-17 11:25:38 +0100

    Update GLogic.cpp

--- GLogic.cpp
+++ GLogic.cpp
@@ -13,14 +13,14 @@ void GLogic::Validmove(int X, int Y)
 {
 	pieces pieces;
 
-	if (boardArray[X][Y] = NULL || pieces.bR || pieces.bN || pieces.bB || pieces.bQ || pieces.bB2 || pieces.bN2 || pieces.bR2 || pieces.bP1 || pieces.bP2 || pieces.bP3 || pieces.bP4 || pieces.bP5 || pieces.bP6 || pieces.bP7 || pieces.bP8);
+	if (boardArray[X][Y] = NULL);
 		validMove = true;
 	if (boardArray[X][Y] = pieces.bK)
 		done = true;
 
 }
 
-void GLogic::ValidmovePawn(int X, int Y)
+void GLogic::ValidMoveEnemy(int X, int Y)
 {
 	pieces pieces;
 
