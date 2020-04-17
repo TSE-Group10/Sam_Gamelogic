8f9b84b6e19f05f135d79ff2adcc54b8d1353ae9
SamMcCullough1213 <61021204+SamMcCullough1213@users.noreply.github.com>
2020-04-17 11:55:40 +0100

    Update GLogic.cpp

--- GLogic.cpp
+++ GLogic.cpp
@@ -1,3 +1,5 @@
+#include "pch.h"
+
 #include "GLogic.h"
 #include "Pieces.h"
 #include <cstdlib>
@@ -13,8 +15,8 @@ void GLogic::Validmove(int X, int Y)
 {
 	pieces pieces;
 
-	if (boardArray[X][Y] = NULL);
-		validMove = true;
+	if (boardArray[X][Y] = NULL)
+	validMove = true;
 	if (boardArray[X][Y] = pieces.bK)
 		done = true;
 
@@ -24,7 +26,7 @@ void GLogic::ValidMoveEnemy(int X, int Y)
 {
 	pieces pieces;
 
-	if (boardArray[X][Y] = pieces.bR || pieces.bN || pieces.bB || pieces.bQ || pieces.bB2 || pieces.bN2 || pieces.bR2 || pieces.bP1 || pieces.bP2 || pieces.bP3 || pieces.bP4 || pieces.bP5 || pieces.bP6 || pieces.bP7 || pieces.bP8);
+	if (boardArray[X][Y] = pieces.bR || pieces.bN || pieces.bB || pieces.bQ || pieces.bB2 || pieces.bN2 || pieces.bR2 || pieces.bP1 || pieces.bP2 || pieces.bP3 || pieces.bP4 || pieces.bP5 || pieces.bP6 || pieces.bP7 || pieces.bP8)
 	validMove = true;
 	if (boardArray[X][Y] = pieces.bK)
 		done = true;
