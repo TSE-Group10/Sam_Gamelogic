8cec0a228a2b9e98d3a9d76b1bfa77141bec3628
SamMcCullough1213 <61021204+SamMcCullough1213@users.noreply.github.com>
2020-04-20 13:18:06 +0100

    Update Pieces.h

--- Pieces.h
+++ Pieces.h
@@ -16,6 +16,8 @@ public:
 	string PlayerRed;	 //Declaration For red and blue
 	string PlayerBlue;
 
+	std::string boardArray[10][10];
+
 	// Declaration for placeholder piece names
 	string bR = "bR ", bN = "bN ", bB = "bB ", bQ = "bQ ", bK = "bK ", bB2 = "bB2", bN2 = "bN2", bR2 = "bR2", bP1 = "bP1", bP2 = "bP2", bP3 = "bP3", bP4 = "bP4", bP5 = "bP5", bP6 = "bP6", bP7 = "bP7", bP8 = "bP8";
 	string rR = "rR ", rN = "rN ", rB = "rB ", rQ = "rQ ", rK = "rK ", rB2 = "rB2", rN2 = "rN2", rR2 = "rR2", rP1 = "rP1", rP2 = "rP2", rP3 = "rP3", rP4 = "rP4", rP5 = "rP5", rP6 = "rP6", rP7 = "rP7", rP8 = "rP8";
@@ -23,6 +25,14 @@ public:
 	int RndPiece; //random number generator for the AI
 
 	bool pieceTaken = false;
+	bool validMove = false;
+
+	bool done = false;
+	bool PlayerTurn = true; 
+
+	void Validmove(int X, int Y);
+	void ValidMoveEnemy(int X, int Y);
+
 
 	void PieceCollection(); //AI input fed into here for piece selection?
 };
